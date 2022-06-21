038fdeddbe09dc6ee9bf93fa01c30a1f465df7b3
mrawildhan <mrawildhan@gmail.com>
2022-06-21 14:10:52 +0800

    wil films

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import NavigationBar from './components/NavigationBar'
+import Intro from './components/Intro'
+import Trending from './components/Trending'
+import Superhero from './components/Superhero'
+import './style/landingPage.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Intro Section Start */}
+      <div className='myBG'>
+        <NavigationBar />
+        <Intro />
+      </div>
+      {/* Intro Section End */}
+
+      {/* Trending Section Start */}
+      <div className='trending'>
+        <Trending />
+      </div>
+      {/* Trending Section End */}
+
+      {/* Superhero Section Start */}
+      <div className='superhero'>
+        <Superhero />
+      </div>
+      {/* Superhero Section End */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
